4f025f3733bbde2a370ab58b7e7dee374fe0289f
k.mitarai <kazmit299@gmail.com>
2024-12-25 22:06:16 +0900

    ローカル接続設定を作成

---
+++ Unity/Assets/Scripts/VantanConnect/Implements/LocalImplement1.cs
@@ -0,0 +1,26 @@
+﻿using Cysharp.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class LocalAPIImplement : INetworkImplement
+{
+    const string URI = "http://localhost:4649/vc/{Command}";
+
+    class GetAddrResult
+    {
+        public int status;
+        public string address;
+    }
+
+    async public UniTask<string> GetAddress()
+    {
+        string request = URI.Replace("{Command}", "getaddr");
+        string json = await Network.WebRequest.GetRequest(request);
+        var ret = JsonUtility.FromJson<GetAddrResult>(json);
+        return ret.address;
+    }
+}
