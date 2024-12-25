07d65d8f2ce0ab05b643cce99260126428a3e51b
k.mitarai <kazmit299@gmail.com>
2024-12-25 22:09:01 +0900

    DB接続の仕組みを更新

--- NodeJSServer/ts/lib/database.ts
+++ NodeJSServer/ts/lib/database.ts
@@ -1,8 +1,17 @@
 //DBを選択
-import { execQuery } from "./dbmodule/mariadb";
+import { connect as dbConnect, execQuery } from "./dbmodule/mariadb";
 //import { execQuery } from "./dbmodule/sqlite3";
+
+export async function connect()
+{
+	try {
+	 	await dbConnect();
+	 }catch(ex){
+	 	console.log(ex);
+	 }
+}
 
 export async function query(query: string, values: any)
 {
-  return await execQuery(query, values);
+ 	return await execQuery(query, values);
 }
--- NodeJSServer/ts/lib/dbmodule/mariadb.ts
+++ NodeJSServer/ts/lib/dbmodule/mariadb.ts
@@ -1,24 +1,39 @@
+import { DATABASE_HOST, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_SELECT } from "./../../config/config"
+
 const mariadb = require('mariadb');
-const pool = mariadb.createPool({host: "localhost", user: process.env.DB_USER, password: process.env.DB_PWD, connectionLimit: 5});
+
+const pool = mariadb.createPool({
+	host: DATABASE_HOST, 
+	user: DATABASE_USERNAME, 
+	password: DATABASE_PASSWORD, 
+	database: DATABASE_SELECT,
+	connectionLimit: 5
+});
+
 let conn: any = null;
 
-async function connect()
+export async function connect()
 {
-  if(conn) return ;
-  
-  conn = await mariadb.createConnection({
-      host: "localhost",
-      user: process.env.DB_USER,
-      password: process.env.DB_PWD,
-      database: "vtn-game-sample"
-      //timezone: 'Asia/Tokyo',
-      //skipSetTimezone: true
-  });
+	if(conn) return ;
+
+	conn = await pool.getConnection();
+	
+	return;
+	let cnInfo = {
+		host: DATABASE_HOST,
+		user: DATABASE_USERNAME,
+		password: DATABASE_PASSWORD,
+		database: DATABASE_SELECT
+		//timezone: 'Asia/Tokyo',
+		//skipSetTimezone: true
+	};
+	console.log(cnInfo);
+	conn = await mariadb.createConnection(cnInfo);
 }
 
 export async function execQuery(query: string, values: any)
 {
-  await connect();
-  const res = await conn.query(query, values);
-  return res;
+	await connect();
+	const res = await conn.query(query, values);
+	return res;
 }
