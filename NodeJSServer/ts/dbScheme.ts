0a2c3fbf526e0106187b5d082716c9acbf8065e8
k.mitarai <kazmit299@gmail.com>
2024-12-25 16:23:55 +0900

    不要ファイル削除

--- NodeJSServer/ts/dbScheme.ts
+++
@@ -1,6 +0,0 @@
-exports.Scheme = {
-	User: { "Version": 2, Scheme: ["id", "udid", "name"] },
-	UserCards: { "Version": 2, Scheme: ["id", "userId", "cardId", "level","luck"] },
-	UserItems: { "Version": 1, Scheme: ["id", "userId", "itemId", "amount"] },
-	UserQuests: { "Version": 1, Scheme: ["id", "userId", "questId", "score","clearFlag"] },
-}
--- NodeJSServer/ts/firebaseconfig.ts
+++
@@ -1,9 +0,0 @@
-exports.config = {
-  apiKey: "AIzaSyDfhmcElYMzZo63Jh5EU2-s_k7AtjaCD48",
-  authDomain: "vtn-socialgame.firebaseapp.com",
-  projectId: "vtn-socialgame",
-  storageBucket: "vtn-socialgame.appspot.com",
-  messagingSenderId: "233973544679",
-  appId: "1:233973544679:web:03839d3bcef6fe7cca1442",
-  measurementId: "G-M104DG5J42"
-};
