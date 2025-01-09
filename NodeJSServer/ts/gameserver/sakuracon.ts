50018b8b810a3ec82fbb77e6c1e8e9a22d3e1a54
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2025-01-09 06:58:24 +0000

    update: ユニークユーザー取得メソッドの組み込み

--- NodeJSServer/ts/gameserver/sakuracon.ts
+++ NodeJSServer/ts/gameserver/sakuracon.ts
@@ -1,11 +1,9 @@
-import { WebSocket, WebSocketServer } from "ws";
 import { GameConnect, createMessage, TARGET, CMD } from "./gamecon";
 import { MessagePacket } from "./../vclogic/vcmessage";
 import { query } from "./../lib/database";
 import crypto from "crypto";
 
-const delay = (waitTime: number) =>
-    new Promise((resolve) => setTimeout(resolve, waitTime));
+const delay = (waitTime: number) => new Promise((resolve) => setTimeout(resolve, waitTime));
 
 /**
  * @summary BOT的なユーザのふるまいをするサービス
@@ -36,10 +34,7 @@ export class SakuraConnect implements MessagePacket {
     private async getUniqueUsers(userId: string = crypto.randomInt(1, 1000).toString()): Promise<any> {
         // TODO: ts/vclogic/vcuser.tsにユニークユーザーを格納している変数があるので、それを使ってもいいかもしれない
         try {
-            let botUserData = await query(
-                "SELECT * FROM User INNER JOIN UserGameStatus ON User.Id = UserGameStatus.UserId WHERE Id < ?",
-                [999]
-            ); 
+            let botUserData = await query("SELECT * FROM User INNER JOIN UserGameStatus ON User.Id = UserGameStatus.UserId WHERE Id < ?", [999]);
             let users: any = [];
             users = botUserData;
 
@@ -54,28 +49,21 @@ export class SakuraConnect implements MessagePacket {
      * @param waitTime 待ち時間 (ミリ秒)
      * @param toUserId 送信先ユーザID
      */
-    public async sakuraConnect(waitTime: number = 0, toUserId: number) {
+    public async sakuraConnect(waitTime: number = 0, toUserId: string) {
         // 応援メソッド発火
         // 引数のwaitTimeでデフォルト即時実行と、待ち時間実行を選択できる
         await delay(waitTime);
 
-        // NOTE: ここでデータベースからBOTユーザーのデータを読み込む
+        // ユニークユーザを取得
+        let botUserData = toUserId ? this.getUniqueUsers(toUserId) : this.getUniqueUsers();
 
-        // これは仮データ
-        let botUserData: MessagePacket = {
-            ToUserId: -1,
-            FromUserId: -1,
-            Name: "サクラ",
-            Message: "サクラです。よろしくお願いします。",
-        };
-        this.ToUserId = botUserData.ToUserId;
-        this.FromUserId = botUserData.FromUserId;
-        this.Name = botUserData.Name;
-        this.Message = botUserData.Message;
+        // TODO: MessagePacketを使うかどうかを検討する
+        // this.ToUserId = botUserData.ToUserId;
+        // this.FromUserId = botUserData.FromUserId;
+        // this.Name = botUserData.Name;
+        // this.Message = botUserData.Message;
 
         // NOTE: ここで応援処理を呼び出す
-        createMessage(this.ToUserId.toString(), CMD.WELCOME, TARGET.OTHER, {
-            UserId: toUserId,
-        });
+        createMessage(toUserId, CMD.WELCOME, TARGET.OTHER, botUserData);
     }
 }
