43ac5f9f23ca8fff8f09867d58827ebc4a997834
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2025-01-16 06:57:53 +0000

    add: 送信データのテンプレート追加

--- NodeJSServer/ts/gameserver/sakuracon.ts
+++ NodeJSServer/ts/gameserver/sakuracon.ts
@@ -1,37 +1,57 @@
 import { GameConnect, createMessage, TARGET, CMD } from "./gamecon";
-import { MessagePacket } from "./../vclogic/vcmessage";
 import { query } from "./../lib/database";
 import crypto from "crypto";
 
-const delay = (waitTime: number) => new Promise((resolve) => setTimeout(resolve, waitTime));
+/**
+ * @summary サクラが送信する応援メッセージ一覧
+ */
+const sakuraSupportMessage: string[] = [
+    "support message test"
+];
+
+const sakuraWelcomeMessage: string[] = [
+    "welcome message test"
+];
+
+/**
+ * @summary メッセージテンプレート
+ */
+interface MessageTemplate {
+    Target: number;
+    // NOTE: 送信されるコマンドの種類
+    // CMD WELCOME: 1, JOIN: 2, EVENT: 3, SEND_JOIN: 100, SEND_EVENT: 101,
+    Command: 1 | 2 | 3 | 100 | 101;
+    Data: {
+        EventId: number;
+        FromId: number | 100; // 100はサクラのユーザID
+        Payload: {
+            Key: string;
+            TypeName: string;
+            Data: string;
+        }[];
+    };
+    SessionId: string; // サクラには不要
+};
 
 /**
  * @summary BOT的なユーザのふるまいをするサービス
  */
-export class SakuraConnect implements MessagePacket {
+export class SakuraConnect {
     // games: any;
     // sessionDic: any;
     // broadcast: any;
 
-    ToUserId: number;
-    FromUserId: number;
-    Name: string;
-    Message: string;
+    private sakuraMessage: MessageTemplate = {} as MessageTemplate;
 
     public constructor() {
-        // MessagePacketの初期化
-        this.ToUserId = -1;
-        this.FromUserId = -1;
-        this.Name = "";
-        this.Message = "";
     }
 
     /**
      * @summary ユニークユーザを取得するメソッド
      * @param {number} userId 取得したいユニークユーザのID (デフォルトはランダム)
-     * @returns {Promise<any>} ユニークユーザの情報
+     * @returns ユニークユーザの情報
      */
-    private async getUniqueUsers(userId: string = crypto.randomInt(1, 1000).toString()): Promise<any> {
+    private async getUniqueUsers(userId: string = crypto.randomInt(1, 1000).toString()) {
         // TODO: ts/vclogic/vcuser.tsにユニークユーザーを格納している変数があるので、それを使ってもいいかもしれない
         try {
             let botUserData = await query("SELECT * FROM User INNER JOIN UserGameStatus ON User.Id = UserGameStatus.UserId WHERE Id < ?", [999]);
@@ -46,24 +66,42 @@ export class SakuraConnect implements MessagePacket {
 
     /**
      * @summary サクラ機能を提供するメソッド
-     * @param waitTime 待ち時間 (ミリ秒)
-     * @param toUserId 送信先ユーザID
+     * @param {number} waitTime 待ち時間 (ミリ秒)
+     * @param {string} toUserId 送信先ユーザID
      */
     public async sakuraConnect(waitTime: number = 0, toUserId: string) {
-        // 応援メソッド発火
-        // 引数のwaitTimeでデフォルト即時実行と、待ち時間実行を選択できる
-        await delay(waitTime);
+        try {
+            console.time("sakuraConnectTimer");
 
-        // ユニークユーザを取得
-        let botUserData = toUserId ? this.getUniqueUsers(toUserId) : this.getUniqueUsers();
+            // 応援メソッド発火
+            // 引数のwaitTimeでデフォルト即時実行と、待ち時間実行を選択できる
+            await this.delay(waitTime);
 
-        // TODO: MessagePacketを使うかどうかを検討する
-        // this.ToUserId = botUserData.ToUserId;
-        // this.FromUserId = botUserData.FromUserId;
-        // this.Name = botUserData.Name;
-        // this.Message = botUserData.Message;
+            console.timeEnd("sakuraConnectTimer");
 
-        // NOTE: ここで応援処理を呼び出す
-        createMessage(toUserId, CMD.WELCOME, TARGET.OTHER, botUserData);
+            // ユニークユーザを取得
+            let botUserData;
+            if (toUserId || toUserId == "-1") {
+                // データベースはIDが1から始まるが、配列は0から始まるため、-1する
+                let parseId = (parseInt(toUserId) - 1).toString();
+                botUserData = this.getUniqueUsers(parseId);
+            } else {
+                botUserData = this.getUniqueUsers();
+            }
+            console.log(botUserData);
+
+            // NOTE: ここで応援処理を呼び出す
+            createMessage(toUserId, CMD.WELCOME, TARGET.OTHER, botUserData);
+        } catch (ex) {
+            console.log(ex);
+        }
+    }
+
+    /**
+     * @summary 遅延処理を行うメソッド
+     * @param {number} ms 遅延時間 (ミリ秒)
+     */
+    private delay(ms: number) {
+        return new Promise((resolve) => setTimeout(resolve, ms));
     }
 }
